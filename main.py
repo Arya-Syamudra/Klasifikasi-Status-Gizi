ba417e862b38a2279413426b2a9e057727bcbdb9
Arya Syamudra <aryasyamudra24@gmail.com>
2025-01-03 11:54:06 +0800

    Add Dependencies

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-"""
-Modul utama untuk menjalankan aplikasi Flask dalam Sistem Pakar Proker.
-"""
-
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=False, host="0.0.0.0", port=5000)
